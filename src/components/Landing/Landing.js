db2e8348bccdbf33ef1390c2b81d62a681d321c0
bxker <jake.luke.baker@gmail.com>
2020-02-11 12:14:16 -0600

    adjusted color scheme

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/Landing.css';
 import LandingNav from './LandingNav/LandingNav';
+import { Link } from 'react-router-dom';
 
 export default function Landing() {
     return (
@@ -9,8 +10,17 @@ export default function Landing() {
             <section className="landing-container">
                 <h2>Welcome to</h2>
                 <h1>Bvdget</h1>
-                <button>Get Started</button>
+                <Link to="/register"><button>Get Started</button></Link>
             </section>
+            <section className="landing-art">
+
+            </section>
+            <section className="landing-art">
+
+            </section>
+            <footer className="footer-main">
+
+            </footer>
         </div>
     )
 }
--- src/components/Landing/LandingNav/LandingNav.js
+++ src/components/Landing/LandingNav/LandingNav.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './styles/LandingNav.css';
+import { Link } from 'react-router-dom';
 
 export default function LandingNav() {
     return (
         <div className="landing-nav">
-            <button>Login</button>
+            <Link to="/register"><button>Register</button></Link>
+            <Link to="/login"><button>Login</button></Link>
         </div>
     )
 }
